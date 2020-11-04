24d3b6b62c4fd073fea25530652b56d350f03905
Ale <alessandrochb@gmail.com>
2020-11-04 16:31:04 +0100

    snack--2 chiedo due numeri all'user + controlli

--- Jsnack2/main.js
+++ Jsnack2/main.js
@@ -8,7 +8,25 @@ Creare un nuovo array con i valori che si trovano in una
 posizione compresa tra i due numeri inseriti dall’utente.
 */
 
-    
+const names = ['pippo', 'mario', 'luca', 'pluto', 'maria', 'marco', 'paola', 'giovanna', 'piera', 'mimmo'];
 
+let rangeNumbers = [];
 
+//CHIEDO DUE NUMERI ALL'UTENTE + CONTROLLI
+do {
+        var userNumbers = parseInt(prompt('inserisci un numero da 1 a 10'));
+        if (isNaN(userNumbers) || userNumbers <= 0 || userNumbers > 10) {
+            alert('numero non valido')
+        } else {
+            rangeNumbers.push(userNumbers - 1);
+            // metto userNumbers - 1 in modo da farlo combaciare con le posizioni dell'array in conteggio "umano"
+        }
+
+    } while ((isNaN(userNumbers) || userNumbers < 0 || userNumbers > 10 || rangeNumbers.length < 2));
+
+const nameRange = name.filter((element, index) =>);
+
+    if (element) {
+
+    }
 });
