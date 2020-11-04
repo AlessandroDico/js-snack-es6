8b4105756dd2112f90ea469488ec2f86e944664a
Ale <alessandrochb@gmail.com>
2020-11-04 15:07:16 +0100

    aggiunte traccie esercizi

--- Jsnack2/main.js
+++ Jsnack2/main.js
@@ -2,5 +2,13 @@ $(document).ready(function(){
 
     console.log('hello');
 
+/*
+Creare un array di nomi e chiedere all’utente due numeri.
+Creare un nuovo array con i valori che si trovano in una
+posizione compresa tra i due numeri inseriti dall’utente.
+*/
+
+    
+
 
 });
--- Jsnack3/main.js
+++ Jsnack3/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
 
     console.log('hello');
+    
+/*
+A partire da un array di oggetti, creare una copia dell’array e
+aggiungere ai singoli elementi dell’array una nuova
+proprietà “position” che contiene una lettera casuale.
+*/
+
 
 
 });
